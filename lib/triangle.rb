f92e592e783c27baabc38c91fe86c48fe4591e14
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-04-03 11:12:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
-  # write code here
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    if @side_one == @side_two == @side_three
+      :equilateral
+    end
+  end
 end
